38aaafbbd1242a731233fe3939b9a74a917ba1b4
Victor Diez <victor.diez@sonarsource.com>
2024-09-26 17:00:18 +0200

    Add unit tests

---
+++ its/eslint-plugin-sonarjs/index.test.js
@@ -0,0 +1,33 @@
+const { test } = require('node:test');
+const assert = require('node:assert');
+const spawn = require('cross-spawn');
+
+function verifyErrors(output) {
+  console.log(output);
+  const errorLines = output.split('\n').filter(line => line.includes('error'));
+  assert(errorLines.length >= 8);
+}
+
+test('should work with CommonJS config', async t => {
+  const result = spawn.sync('npx', ['eslint', '-c', 'eslint.config.cjs', 'file.js'], {
+    cwd: __dirname,
+    encoding: 'utf-8',
+  });
+  verifyErrors(result.stdout);
+});
+
+test('should work with ECMAScript modules config', async t => {
+  const result = spawn.sync('npx', ['eslint', '-c', 'eslint.config.mjs', 'file.js'], {
+    cwd: __dirname,
+    encoding: 'utf-8',
+  });
+  verifyErrors(result.stdout);
+});
+
+test('should work with TSESLint config', async t => {
+  const result = spawn.sync('npx', ['eslint', '-c', 'tseslint.config.mjs', 'file.ts'], {
+    cwd: __dirname,
+    encoding: 'utf-8',
+  });
+  verifyErrors(result.stdout);
+});
---
+++ its/eslint-plugin-sonarjs/tests/S6661/unit.test.ts
@@ -0,0 +1,91 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import { RuleTester } from 'eslint';
+import { rule } from './';
+import path from 'path';
+
+process.chdir(__dirname); // change current working dir to avoid the package.json lookup to up in the tree
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 } });
+
+ruleTester.run('Object spread syntax should be used instead of "Object.assign"', rule, {
+  valid: [
+    {
+      code: `Object.assign(foo, bar);`,
+    },
+  ],
+  invalid: [
+    {
+      code: `const a = Object.assign({}, foo);`,
+      output: `const a = { ...foo};`,
+      errors: [
+        {
+          line: 1,
+          endLine: 1,
+          column: 11,
+          endColumn: 24,
+        },
+      ],
+    },
+    {
+      code: `const b = Object.assign({}, foo, bar);`,
+      output: `const b = { ...foo, ...bar};`,
+      errors: [
+        {
+          line: 1,
+          endLine: 1,
+          column: 11,
+          endColumn: 24,
+        },
+      ],
+    },
+    {
+      code: `
+var assign = Object.assign;
+const b = assign({}, foo, bar);`,
+      output: `
+var assign = Object.assign;
+const b = { ...foo, ...bar};`,
+      errors: [
+        {
+          line: 3,
+          endLine: 3,
+          column: 11,
+          endColumn: 17,
+        },
+      ],
+    },
+  ],
+});
+
+const filename = path.join(__dirname, 'fixtures', 'unsupported-node', 'file.js');
+
+ruleTester.run(
+  'When the project does not support the object spread syntax, the rule should be ignored',
+  rule,
+  {
+    valid: [
+      {
+        code: `Object.assign({}, bar);`,
+        filename,
+      },
+    ],
+    invalid: [],
+  },
+);
